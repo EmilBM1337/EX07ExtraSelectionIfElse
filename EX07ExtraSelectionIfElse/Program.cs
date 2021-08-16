2cf6073b6c08e38d7b176fb1ef0d078ca51065b5
Emil Bang Mathiesen <emil41m5@edu.campusvejle.dk>
2021-08-16 10:22:19 +0200

    Add project files.

---
+++ EX07ExtraSelectionIfElse/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EX07ExtraSelectionIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Velkommen til mit program!");
+            Console.WriteLine("");
+            Console.WriteLine("Tast et bogstav for det du ønsker, og afslut med enter.");
+            Console.WriteLine("a) Velkomstbesked");
+            Console.WriteLine("b) Udregning af BMI");
+            Console.WriteLine("c) Størst af to tal");
+
+            string a;
+            string b;
+            string c;
+        }
+    }
+}
